ed3c6c48fa2eee61d6fd4059554ebac478a7a872
Jerry Dobner <jdobner@gmail.com>
2020-12-24 09:41:19 -0500

    classes

--- pycls/pycls.py
+++ pycls/pycls.py
@@ -10,10 +10,18 @@ class MyClass:
     def staticmethod():
         return 'static method called'
 
+    @staticmethod
+    def fun():
+        pass
+
 mc = MyClass()
 print(mc.method())
+print(mc.staticmethod())
 print(MyClass.staticmethod())
 print(MyClass.classmethod())
 
 f = MyClass.classmethod
 print(f())
+print(MyClass.method(mc))
+#print(MyClass().plain_method())
+print(MyClass.method("BOOOO"))
