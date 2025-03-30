4767fff4913603f8967cfe8b0584b365d7cd6f46
Jerry Dobner <jdobner@curbngo.com>
2025-03-29 21:24:43 -0400

    min_subarrays

---
+++ min_subarray.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        smallest = 0
+        current = 0
+        len = len(nums)
+
+        for right in range(0, len):
+            current += nums[right]
+            if 
+            if right - left:
+
+
+
