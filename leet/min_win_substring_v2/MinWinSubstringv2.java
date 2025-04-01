6fbf9c2f81e2bfdb9e12b3bf36634adad5c588e6
Jerry Dobner <jdobner@curbngo.com>
2025-04-01 10:30:29 -0400

    latest

--- leet/min_win_substring_v2/MinWinSubstringv2.java
+++ leet/min_win_substring_v2/MinWinSubstringv2.java
@@ -2,10 +2,10 @@ package leet.min_win_substring_v2;
 
 class Solution {
     public String minWindow(String s, String t) {
-        var arr = new int['z' + 1];
-        //int ayy = 'A';
+        var arr = new int['z' - 'A' + 1];
+        int ayy = 'A';
         for (char c : t.toCharArray()) {
-            arr[c]++;
+            arr[c - ayy]++;
         }
         var target = t.length();
         var minLen = s.length() + 1;
@@ -17,14 +17,14 @@ class Solution {
 
         var left = 0;
         for (var right = 0; right < s.length(); right++) {
-            //int index = s.charAt(right) - ayy;
-            if (--arr[s.charAt(right)] >= 0) {
+            int index = s.charAt(right) - ayy;
+            if (--arr[index] >= 0) {
                 target--;
             }
             if (target == 0) {
                 for (; target == 0; left++) {
-                    //int index2 = s.charAt(left) - ayy;
-                    if (++arr[s.charAt(left)] > 0) {
+                    int index2 = s.charAt(left) - ayy;
+                    if (++arr[index2] > 0) {
                         target++;
                         var len = right - left + 1;
                         if (len < minLen) {
@@ -39,8 +39,9 @@ class Solution {
     }
 
     void test(String s, String t, String r) {
-        assert this.minWindow(s, t) == r;
-        System.out.println(s + ", " + t + " = " + r);
+        var result = this.minWindow(s, t);
+        System.out.println(s + ", " + t + " = " + result);
+        assert result.equals(r);
     }
 
     public static void main(String[] args) {
